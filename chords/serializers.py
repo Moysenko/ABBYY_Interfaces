e431b4d1741b218f7c6b3c9365e6aef99dcd0ec0
Mikhail Moysenko <moysenkom@yandex.ru>
2023-05-09 16:11:10 +0300

    update gh-pages

--- chords/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Chord
-from app.serializers import UserSerializer
-
-class ChordSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Chord
-    fields = ['id', 'name', 'image']
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import { songs } from './songs';
-import { chords } from  './chords'
-
-export const reducers = combineReducers({ songs, chords});
